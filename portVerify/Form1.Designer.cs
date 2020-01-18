071e6923f1457e6c16f85862e8e23cb083ca19aa
Deng Yu <jardy@vip.qq.com>
2020-01-18 17:08:59 +0800

    添加项目文件。

---
+++ portVerify/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace portVerify
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtPort);
+            this.groupBox1.Controls.Add(this.txtIP);
+            this.groupBox1.Controls.Add(this.lblPort);
+            this.groupBox1.Controls.Add(this.lblIP);
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.groupBox1.Location = new System.Drawing.Point(37, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(459, 89);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "远程服务端信息";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtPort.ForeColor = System.Drawing.Color.Blue;
+            this.txtPort.Location = new System.Drawing.Point(311, 40);
+            this.txtPort.MaxLength = 5;
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(65, 25);
+            this.txtPort.TabIndex = 3;
+            this.txtPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtIP.ForeColor = System.Drawing.Color.Red;
+            this.txtIP.Location = new System.Drawing.Point(94, 40);
+            this.txtIP.MaxLength = 15;
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(127, 25);
+            this.txtIP.TabIndex = 2;
+            this.txtIP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblPort.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lblPort.Location = new System.Drawing.Point(265, 43);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(40, 16);
+            this.lblPort.TabIndex = 1;
+            this.lblPort.Text = "端口";
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblIP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lblIP.Location = new System.Drawing.Point(32, 43);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(56, 16);
+            this.lblIP.TabIndex = 0;
+            this.lblIP.Text = "IP地址";
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnDetect.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnDetect.Location = new System.Drawing.Point(549, 56);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(103, 66);
+            this.btnDetect.TabIndex = 1;
+            this.btnDetect.Text = "检测";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 168);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(697, 322);
+            this.txtResult.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 502);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnDetect);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "数据链路连通性测试工具（IFC专用）";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ portVerify/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows.Forms;
+using System.Text;
+using System.Threading;
+
+namespace portVerify
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDetect_Click(object sender, EventArgs e)
+        {
+            btnDetect.Enabled = false;
+            string ip = txtIP.Text.Trim();
+            int port;
+            bool isPort = false;
+            isPort = Int32.TryParse(txtPort.Text.Trim(), out port);
+            string regformat = @"^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$";
+            Regex regex = new Regex(regformat, RegexOptions.IgnoreCase);
+            if (!string.IsNullOrEmpty(ip) && ip.Length >= 7 && ip.Length <= 15 && regex.IsMatch(ip))
+            {
+                if (isPort && port >= 0 && port <= 65535)
+                {
+                    //验证通过
+                    try
+                    {
+                        Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                        socket.Connect(IPAddress.Parse(ip), port);
+                        txtResult.AppendText(string.Format("检测到该通道（{0}:{1}）可用，返回数据：", ip, port));
+                        Thread thread = new Thread(new ParameterizedThreadStart(ReceivedData));
+                        thread.IsBackground = true;
+                        thread.Start(socket);
+
+                    }
+                    catch(Exception ex)
+                    {
+                        txtResult.AppendText("检测到该通道不可用，报错信息：\n" + ex.ToString() + "\n");
+                        btnDetect.Enabled = true;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("您输入的端口不规范，请重新输入\n", "验证失败", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    btnDetect.Enabled = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("您输入的IP地址不规范，请重新输入\n", "验证失败", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                btnDetect.Enabled = true;
+            }
+        }
+
+        public void ReceivedData(object socket)
+        {
+            var proxSocket = socket as Socket;
+            byte[] data = new byte[1024 * 1024];
+            //while (true)
+            {
+                int len = proxSocket.Receive(data, 0, data.Length, SocketFlags.None);
+                string recStr = Encoding.Default.GetString(data, 0, len);
+                lblIP.Invoke(new Action<string>(s =>
+                {
+                    txtResult.AppendText(s + "\n");
+                    btnDetect.Enabled = true;
+                }), recStr);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtResult.AppendText("验证工具初始化完成...\n");
+            try
+            {
+                string HostName = Dns.GetHostName();
+                IPHostEntry IpEntry = Dns.GetHostEntry(HostName);
+                for (int i = 0; i < IpEntry.AddressList.Length; i++)
+                {
+                    if (IpEntry.AddressList[i].AddressFamily == AddressFamily.InterNetwork)
+                        txtResult.AppendText(string.Format("本机IP地址：{0}\n", IpEntry.AddressList[i].ToString()));
+                }
+            }
+            catch (Exception ex)
+            {
+                txtResult.AppendText(ex.ToString());
+            }
+        }
+    }
+}
